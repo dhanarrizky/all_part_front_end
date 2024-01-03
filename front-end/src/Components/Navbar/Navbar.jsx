3295139a5314f23444716e097aae2a72756bec58
dhanarrizky <dhanarrizky23@gmail.com>
2024-01-03 10:02:16 +0700

    updateing some css on my sidebar

--- front-end/src/Components/Navbar/Navbar.jsx
+++ front-end/src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import './Navbar.css';
 import Data from '../../assets/datanav';
 import Navitem from "../Navitem/Navitem";
+import { FaChevronCircleLeft, FaChevronCircleRight } from "react-icons/fa";
 
 const Navbar = () => {
+    const[Hid1, setHid1] = useState('')
+    const[Hid2, setHid2] = useState('hidden')
+    const[navcls, setnavcls] = useState('nav')
+
+    const ClickMe = () => {
+        if(Hid1==''){
+            setHid1('hidden')
+            setHid2('')
+            setnavcls('nav nav-hid')
+        } else {
+            setHid1('')
+            setHid2('hidden')
+            setnavcls('nav')
+        }
+    }
 
     return (
-        <div className="nav">
+        <div className={navcls}>
             <div>
                 {Data.map((item) => {
-                    return <Navitem title={item.name} icons={item.icon}  link={item.link}/>
+                    return <Navitem hid={Hid1} title={item.name} icons={item.icon}  link={item.link}/>
                 })}
+            </div>
+            <div className="button-group" onClick={() => ClickMe()}>
+                <FaChevronCircleLeft className={Hid1}/>
+                <FaChevronCircleRight className={Hid2}/>
             </div>
         </div>
     )
--- front-end/src/Components/Navitem/Navitem.jsx
+++ front-end/src/Components/Navitem/Navitem.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './Navitem.css';
 
 
 const Navitem = (promp) => {
+    const[hidd, sethidd] = useState('')
+    const[acls, setacls] = useState('ahid')
+
+    useEffect(()=> {
+        let hid = promp.hid
+        sethidd("title "+hid)
+
+        if(acls === 'ahid-act'){
+            setacls('ahid')
+        }else{
+            setacls('ahid-act')
+        }
+
+        console.log(hidd)
+    },[promp.hid])
+
     return (
         <div className="nav-item">
-            <a href={promp.link}>
+            <a href={promp.link} className={acls}>
                 <div className="icon">
                     {promp.icons}
                 </div>
-                <div className="title">
+                <div className={hidd}>
                     {promp.title}
                 </div>
             </a>
