42142d7ab0b60532f4a89223baf21abf3851360b
dhanarrizky <dhanarrizky23@gmail.com>
2024-01-09 09:11:38 +0700

    createing slide content

---
+++ front-end/src/Components/CardGroup/CardGroup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './CardGroup.css';
+
+const CardGroup = () => {
+    return (
+        <div className="Card-Group">
+
+        </div>
+    )
+}
+
+export default CardGroup;
---
+++ front-end/src/assets/data.js
@@ -0,0 +1,32 @@
+const data = [
+    {
+        id:"1",
+        name:"dhanar"
+    },
+    {
+        id:"2",
+        name:"rizky"
+    },
+    {
+        id:"3",
+        name:"rizky"
+    },
+    {
+        id:"4",
+        name:"krisna"
+    },
+    {
+        id:"5",
+        name:"krisna"
+    },
+    {
+        id:"6",
+        name:"krisna"
+    },
+    {
+        id:"7",
+        name:"dhanar"
+    },
+]
+
+export default data;
